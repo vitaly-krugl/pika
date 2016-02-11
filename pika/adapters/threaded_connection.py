2ce346e5d7b0a15054baf568bf5fb47d5df21334
Vitaly Kruglikov <vkruglikov@numenta.com>
2016-02-11 11:46:12 -0800

    work in progress; events, channel number pool

--- pika/adapters/threaded_connection.py
+++ pika/adapters/threaded_connection.py
@@ -24,7 +24,7 @@ classes.
 
 import logging
 ##import Queue
-##import threading
+import threading
 
 from pika.adapters import blocking_connection_base
 import pika.connection
@@ -118,6 +118,60 @@ class ThreadedConnection(blocking_connection_base.BlockingConnectionBase):
         super(ThreadedConnection, self)._cleanup()
 
 
+class _ThreadSafeChannelNumberPool(object):
+    """Thread-safe channel number allocator"""
+
+    def __init__(self, max_channels):
+
+        self._max_channels = max_channels
+
+        self._lock = threading.Lock()
+
+        # Allocated channel numbers
+        self._allocated_channels = set()
+
+
+    def reserve(self):
+        """Reserve a channel number, making it unavailable until it is returned
+        back to channel number pool via `_ThreadSafeChannelNumberPool.release`
+
+        :returns: channel number
+        :rtype: int
+
+        :raises pika.exceptions.NoFreeChannels: if no more channels are
+            available
+
+        """
+
+        channel_number = None
+
+        with self._lock:
+            if len(self._allocated_channels) >= self._max_channels:
+                raise exceptions.NoFreeChannels()
+
+            for n in xrange(1, len(self._allocated_channels) + 1):
+                if n not in self._allocated_channels:
+                    channel_number = n
+                    break
+            else:
+                channel_number = len(self._allocated_channels) + 1
+
+            self._allocated_channels.add(channel_number)
+
+        return channel_number
+
+    def release(self, channel_number):
+        """Release a channel number, making it available to for allocation
+
+        :param int channel_number: channel number to release; must be in
+            reserved set
+        :raises KeyError: if `channel_number` is not in reserved set
+
+        """
+        with self._lock:
+            self._allocated_channels.remove(channel_number)
+
+
 class _ProxyConnection(pika.connection.Connection):
     """`_ProxyConnection` serves as a proxy for the background AMQP connection.
     It provides the asyncronous ("_impl") connection services expected by
@@ -153,11 +207,64 @@ class _ProxyConnection(pika.connection.Connection):
             on_close_callback=on_close_callback)
 
     def connect(self):
-        """Replace the connection machinery. `_ProxyConnection` connects to
-        AMQP broker indirectly via `_BackgroundConnection`.
+        """[replace base] Replace the connection machinery. `_ProxyConnection`
+        connects to AMQP broker indirectly via `_BackgroundConnectionService`.
 
         This method is invoked by the base `Connection` class to initiate
         connection-establishment with AMQP broker
+
+        """
+        pass
+
+    def _send_connection_close(self, reply_code, reply_text):
+        """[replace base] Send a Connection.Close method frame.
+
+        :param int reply_code: The reason for the close
+        :param str reply_text: The text reason for the close
+
+        """
+        pass
+
+    def add_on_connection_blocked_callback(self,  # pylint: disable=C0103
+                                           callback_method):
+        """[supplement base] Add a callback to be notified when RabbitMQ has
+        sent a `Connection.Blocked` frame indicating that RabbitMQ is low on
+        resources. Publishers can use this to voluntarily suspend publishing,
+        instead of relying on back pressure throttling. The callback will be
+        passed the `Connection.Blocked` method frame.
+
+        :param method callback_method: Callback to call on `Connection.Blocked`,
+            having the signature callback_method(pika.frame.Method), where the
+            method frame's `method` member is of type
+            `pika.spec.Connection.Blocked`
+
+        """
+        pass
+
+    def add_on_connection_unblocked_callback(self,  # pylint: disable=C0103
+                                             callback_method):
+        """[supplement base] Add a callback to be notified when RabbitMQ has
+        sent a `Connection.Unblocked` frame letting publishers know it's ok to
+        start publishing again. The callback will be passed the
+        `Connection.Unblocked` method frame.
+
+        :param method callback_method: Callback to call on
+            `Connection.Unblocked`, having the signature
+            callback_method(pika.frame.Method), where the method frame's
+            `method` member is of type `pika.spec.Connection.Unblocked`
+
+        """
+
+    def channel(self, on_open_callback, channel_number=None):
+        """[supplement base] Create a new channel with the next available
+        channel number or pass in a channel number to use. Must be non-zero if
+        you would like to specify but it is recommended that you let Pika manage
+        the channel numbers.
+
+        :param method on_open_callback: The callback when the channel is opened
+        :param int channel_number: The channel number to use, defaults to the
+                                   next available.
+        :rtype: pika.channel.Channel
 
         """
         pass
@@ -208,31 +315,35 @@ class _ProxyConnection(pika.connection.Connection):
         pass
 
 
-class _BackgroundConnection(object):
-    """`_BackgroundConnection` runs the true AMQP connection instance in a
-    background thread. It communicates with `ThreadedConnection` via thread-safe
-    queues and an inbound event socket for efficient event-driven I/O.
+class _BackgroundConnectionService(object):
+    """`_BackgroundConnectionService` runs the true AMQP connection instance in
+    a background thread. It communicates with `ThreadedConnection` via
+    thread-safe queues and an inbound event socket for efficient event-driven
+    I/O.
 
     """
     pass
 
 
 class _ServiceProxy(object):
-    """Interface to `_BackgroundConnection` for use by `ThreadedConnection`"""
+    """Interface to `_BackgroundConnectionService` for use by
+    `ThreadedConnection`
+    """
 
     def __init__(self, queue, notify):
         """
         :param Queue.Queue queue: Thread-safe queue for depositing events
-            destined for `_BackgroundConnection`
-        :param callable notify: Function for notifying `_BackgroundConnection`
-          that an event is available; it has the signature`notify()`
+            destined for `_BackgroundConnectionService`
+        :param callable notify: Function for notifying
+            `_BackgroundConnectionService` that an event is available; it has
+            the signature`notify()`
 
         """
         self._evt_queue = queue
         self._notify = notify
 
     def send(self, event):
-        """Send an event to `_BackgroundConnection`
+        """Send an event to `_BackgroundConnectionService`
 
         :param event: event object destined for client
 
@@ -242,7 +353,10 @@ class _ServiceProxy(object):
 
 
 class _ClientProxy(object):
-    """Interface to `ThreadedConnection` for use by `_BackgroundConnection`"""
+    """Interface to `ThreadedConnection` for use by
+    `_BackgroundConnectionService`
+
+    """
 
     def __init__(self, queue):
         """
@@ -262,34 +376,59 @@ class _ClientProxy(object):
         self._evt_queue.put(event)
 
 
-class _ClientRegEvent(object):
+class RpcEvent(object):
+    """Base class for service-destined event that solicits a reply"""
+
+    def __init__(self, on_result_rx):
+        """
+        :param callable on_result_rx: callable for handling the result in user
+            context, having the signature `on_result_rx(result)`, where `result`
+            is the value provided by responder.
+        """
+        self.on_result_rx = on_result_rx
+
+        # Result to be set by responder
+        self.result = None
+
+
+class _ClientRegEvent(RpcEvent):
     """Client registration event for registering a client with
-    `_BackgroundConnection`.
+    `_BackgroundConnectionService`.
     """
 
-    def __init__(self, client):
+    def __init__(self, client, on_result_rx):
         """
         :param _ClientProxy:
+        :param callable on_result_rx: callable for handling the result in user
+            context, having the signature `on_result_rx(result)`, where `result`
+            is the value provided by responder.
 
         """
+        super(_ClientRegEvent, self).__init__(on_result_rx)
         self.client = client
 
 
-class _ClientUnregEvent(object):
+class _ClientUnregEvent(RpcEvent):
     """Client de-registration event for unregistering a client with
-    `_BackgroundConnection`.
+    `_BackgroundConnectionService`.
     """
 
-    def __init__(self, client):
+    def __init__(self, client, on_result_rx):
         """
         :param _ClientProxy:
+        :param callable on_result_rx: callable for handling the result in user
+            context, having the signature `on_result_rx(result)`, where `result`
+            is the value provided by responder.
+
         """
+        super(_ClientRegEvent, self).__init__(on_result_rx)
         self.client = client
 
 
 class _ChannelRegEvent(object):
-    """Channel registration event for informing `_BackgroundConnection` of the
-    association between channel number and client
+    """Channel registration event for informing `_BackgroundConnectionService`
+    of the association between channel number and client
+
     """
 
     def __init__(self, client, channel_number):
@@ -303,8 +442,10 @@ class _ChannelRegEvent(object):
 
 
 class _ChannelUnregEvent(object):
-    """Channel de-registration event for informing `_BackgroundConnection` to
-    remove the association between the given channel number and client
+    """Channel de-registration event for informing
+    `_BackgroundConnectionService` to remove the association between the given
+    channel number and client
+
     """
 
     def __init__(self, client, channel_number):
@@ -317,9 +458,9 @@ class _ChannelUnregEvent(object):
         self.client = client
 
 
-class _BlockedConnectionRequestEvent(object):
+class _BlockedConnectionSubscribeEvent(object):
     """Request from a client for receiving "blocked/unblocked" connection
-    frames from `_BackgroundConnection`
+    frames from `_BackgroundConnectionService`
     """
 
     def __init__(self, client):
@@ -330,24 +471,30 @@ class _BlockedConnectionRequestEvent(object):
         self.client = client
 
 
-class _DataToBrokerEvent(object):
+class _DataToBrokerEvent(RpcEvent):
     """Container for serialized frames destined for AMQP broker. This type of
-    event is dispatched by `ThreadedConnection` to `_BackgroundConnection`.
+    event is dispatched by `ThreadedConnection` to
+    `_BackgroundConnectionService`.
+
     """
 
-    def __init__(self, client, buffers):
+    def __init__(self, client, buffers, on_result_rx):
         """
         :param _ClientProxy:
         :param buffers: sequence of serialized frames destined for AMQP broker
+        :param callable on_result_rx: callable for handling the result in user
+            context, having the signature `on_result_rx(result)`, where `result`
+            is the value provided by responder.
 
         """
+        super(_ClientRegEvent, self).__init__(on_result_rx)
         self.client = client
         self.buffers = buffers
 
 
 class _FramesToClientEvent(object):
     """Container for frames destined for client. This type of event is
-    dispatched by `_BackgroundConnection` to `ThreadedConnection`
+    dispatched by `_BackgroundConnectionService` to `ThreadedConnection`
     """
 
     def __init__(self, frames):
